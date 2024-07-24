705ba9e3cafe96d84e41e2726598ff27fb04f636
PokeyLink227 <thomasrice7@yahoo.com>
2024-07-24 17:13:09 -0400

    switched to text analysis

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import re
 import json
 import time
 import random
@@ -7,20 +8,28 @@ import os
 import sys
 from zipfile import ZipFile
 
-import requests
 from PIL import ImageGrab
 from PIL import ImageChops
-import pyautogui
-import screen_brightness_control as sbc
+import pyperclip
 import keyboard
+import pyautogui
+import winsound
+import requests
+import screen_brightness_control as sbc
 
+re_cap = re.compile(r"CA\s+Open")
+re_intl = re.compile(r"DUB|EDI|LHR|LGW|CDG|AMS")
+re_pair = re.compile(r"(Pairing # (?P<tripid>[\w\d]+) :.*?Crew:\s+(?P<cap>Open|.+?\n)(?=(Pairing|$)))")
+par_ignored = ['NONE']
+loc_par_details = [1150, 710]
+loc_par_table = [1500, 500]
 
-def IndexOfImage(imageArray, img):
-    for i in range(len(imageArray)):
-        if not ImageChops.difference(img, imageArray[i]).getbbox():
-            return i
-    return -1
+re_prem = re.compile(r"(^(\w{5})\s+(\w+)\s+\d+\s+(\d{2}:\d{2}).*?X.*?$)")
+prem_ignored = ['NONE']
+loc_prem_refresh = [120, 70]
+loc_prem_table = [600, 600]
 
+night_cont = False
 
 def Pause():
     print('[System] Program paused')
@@ -31,23 +40,12 @@ def Pause():
         quit()
         exit()
 
-def Alert(msg, silent=False):
-    print('[Alert]', msg)
-    if night_cont:
-        sbc.set_brightness(30)
-    winsound.PlaySound('alert_sound.wav', winsound.SND_LOOP|winsound.SND_ASYNC)
-
-    response = pyautogui.confirm(text=msg, title='Select option on how to continue', buttons=['Continue', 'Pause', 'Exit'])
-
-    winsound.PlaySound(None, 0)
-
-    if response == 'Pause':
-        Pause()
-    elif response == 'Exit':
-        os._exit()
-    if night_cont:
-        sbc.set_brightness(0)
-
+def Wait(seconds):
+    target = time.time() + seconds
+    while time.time() < target:
+        if keyboard.is_pressed('p'):
+            Pause()
+        #time.sleep(0.5)
 
 #reload and wait page on left or right of screen (0, 1), return True or False if successful
 def ReloadAndWait(window):
@@ -60,116 +58,98 @@ def ReloadAndWait(window):
 
     time.sleep(0.5)
     while ImageChops.difference(ImageGrab.grab(bbox=bb_reload[window]), img_reload_initial[window]).getbbox() and i > 0:
-        print('[System] {} window has not loaded yet, waiting'.format('Premium' if window == 0 else 'Rsa'), str(i), 'more seconds')
+        print('[System] {} window has not loaded yet, waiting'.format('Left' if window == 0 else 'Right'), str(i), 'more seconds')
         i -= 1
         time.sleep(1)
 
     return i > 10
 
 
-def Wait(seconds):
-    print('[System] Waiting', str(seconds), 'seconds')
-    target = time.time() + seconds
-    while time.time() < target:
-        if keyboard.is_pressed('p'):
-            Pause()
-        #time.sleep(0.5)
+def par_filter(x):
+    if not re_cap.search(x[2]) == None and not x[1] in par_ignored and re_intl.search(x[0]) == None:
+        return True
+    else:
+        return False
 
-#keep track of each rows id and premium status. each iteration retrieve new trip info then for each premium trip found check if it was present in last iteration
-#if not then alert user
-#
-def MonitorWindow():
-    img_names = [0] * prem_num_rows
-    img_icons = [0] * prem_num_rows
-    img_names_last = [0] * prem_num_rows
-    img_icons_last = [0] * prem_num_rows
+def get_pairings():
+    global par_ignored
 
-    img_rsa = [0] * rsa_num_rows
-    img_rsa_last = [0] * rsa_num_rows
+    pyautogui.click(loc_par_details)
+    time.sleep(0.5)
 
-    num_refreshes = 0
+    pyautogui.click(loc_par_table)
+    keyboard.press_and_release('ctrl + a, ctrl + c')
+    time.sleep(0.5)
 
-    pyautogui.click(loc_mousehide[0])
-    for i in range(0, prem_num_rows):
-        img_names[i] = ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i))
-        img_icons[i] = ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i))
+    data = pyperclip.paste()
+    matches = re.findall(r"(Pairing # (?P<tripid>[\w\d]+) :.*?Crew:\s+(?P<cap>Open|.+?\n)(?=(Pairing|$)))", data, re.S)
+    filtered_list = filter(par_filter, matches)
+    trip_ids = [x[1] for x in filtered_list]
+    trip_ids_str = '\n'.join(trip_ids)
 
-    pyautogui.click(loc_mousehide[1])
-    for i in range(0, rsa_num_rows):
-        img_rsa[i] = ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i))
+    par_ignored += trip_ids
 
-    while True:
-        realign()
-        play_alert = False
-        linesfound = []
-        img_names_last = img_names.copy()
-        img_icons_last = img_icons.copy()
-        pyautogui.click(loc_mousehide[0])
-        for i in range(0, prem_num_rows):
-            img_names[i] = ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i))
-            img_icons[i] = ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i))
+    print(trip_ids)
+    if len(trip_ids) == 0:
+        return
 
-            #just look for empty image. ned to make custom index of function to check for image equality.
-            # row contains premium trip where it did not before
-            if ImageChops.difference(img_blank, img_icons[i]).getbbox() or not filter_prem:
-                if filter_prem:
-                    print('[Debug] Trip on row', str(i), 'is premium, previously found on row ', end='')
-                    img_old_index = IndexOfImage(img_names_last, img_names[i])
-                    print(str(img_old_index))
-                # trip existed before but was not premium
-                if img_old_index > -1:
-                    if not ImageChops.difference(img_icons_last[img_old_index], img_blank).getbbox():
-                        play_alert = True
-                        if filter_prem:
-                            linesfound.append(i + 1)
-                        #Alert("Trip went Premium")
-                    #else:
-                        #Alert('Trip stayed premium')
-                else:
-                    play_alert = True
-                    linesfound.append(i + 1)
-                    #Alert("New Premium trip added")
+    winsound.PlaySound('alert_sound.mp3', winsound.SND_LOOP|winsound.SND_ASYNC)
+    choice = pyautogui.confirm(text=f"found {len(trip_ids)} trips\n" +trip_ids_str, title='Parings Found', buttons=['Highlight Next', 'Ignore All'])
+    winsound.PlaySound(None, 0)
 
-        if play_alert:
-            Alert('Premium trip found on line(s) {}'.format(str(linesfound)))
-            play_alert = False
+    if choice == 'Ignore All':
+        return
 
-        print(f'[System] Refresh count: {num_refreshes}')
-        Wait(random.randrange(2, 6))
-        if num_refreshes % 15 == 14:
-            ReloadAndWait(0)
+    for trip in trip_ids:
+        pyautogui.click(loc_par_table)
+        pyperclip.copy("Pairing # " + trip)
+        keyboard.press_and_release('ctrl + f, ctrl + v, enter')
+        choice = pyautogui.confirm(text="Currently highlighted " + trip, title='Pairings Found', buttons=['Highlight Next', 'Ignore Remaining'])
 
-        num_refreshes = num_refreshes + 1
-
-        if option_monitor_rsa:
-            realign()
-            img_rsa_last = img_rsa.copy()
-            pyautogui.click(loc_mousehide[1])
-            for i in range(0, rsa_num_rows):
-                img_rsa[i] = ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i))
-                img_old_index = IndexOfImage(img_rsa_last, img_rsa[i])
-                if img_old_index == -1 and ImageChops.difference(rsa_blank, img_rsa[i]).getbbox():
-                    play_alert = True
-                    linesfound.append(i + 1)
-
-            if play_alert:
-                Alert('Rsa found on line(s) {}'.format(str(linesfound)))
-                play_alert = False
-
-            if num_refreshes % rsa_interval == 0 and option_monitor_rsa:
-                print(f'[System] Refresh count: {num_refreshes}')
-                Wait(random.randrange(10, 50))
-                ReloadAndWait(1)
-                num_refreshes = num_refreshes + 1
-
-def realign():
-    return
-    img_horizprobe = ImageGrab.grab(bbox=(0, bb_prem_icon[1], 600, bb_prem_icon[1] + 1))
-    for x in range(560, 450, -1):
-        if img_horizprobe.getpixel((x, 0)) == (128, 128, 128):
-            bb_prem_icon[0] = x - 20
-            bb_prem_icon[2] = x - 10
+        if choice == 'Ignore Remaining':
             break
+
+    choice = pyautogui.confirm(text="Would you like to resume", title='Parings Found', buttons=['Continue', 'Pause', 'Quit'])
+
+    if choice == 'Quit':
+        quit()
+    elif choice == 'Pause':
+        Pause()
+
+def prem_filter(x):
+    if re_intl.search(x[0]) == None and not x[1] in prem_ignored:
+        return True
+    else:
+        return False
+
+def get_premium():
+    global prem_ignored
+
+    # refresh and wait
+    pyautogui.click(loc_prem_refresh)
+    time.sleep(2)
+
+    pyautogui.click(loc_prem_table)
+    keyboard.press_and_release('ctrl + a, ctrl + c')
+    time.sleep(0.5)
+
+    data = pyperclip.paste()
+    matches = re.findall(r"(^(\w{5})\s+(\w+)\s+\d+\s+(\d{2}:\d{2}).*?X.*?$)", data, re.M)
+    filtered_list = filter(prem_filter, matches)
+
+    trip_id_dates = [x[1] + " " + x[2] for x in filtered_list]
+    trips = '\n'.join(trip_id_dates)
+    prem_ignored += trip_id_dates
+
+    if len(trip_id_dates) == 0:
+        return
+
+    winsound.PlaySound('alert_sound.mp3', winsound.SND_LOOP|winsound.SND_ASYNC)
+    choice = pyautogui.confirm(text=f"found {len(trip_id_dates)} trips\n" + trips, title='Premium Found', buttons=['Ignore', 'Pause', 'Quit'])
+    winsound.PlaySound(None, 0)
+
+    if choice == 'Pause':
+        Pause()
 
 def update():
     print(f'[System] Checking for updates, current version {VERSION}')
@@ -202,24 +182,30 @@ def update():
         sys.exit()
     print('[System] Program up to date')
 
+def main():
+    time.sleep(5)
+
+    cycle = 0
+
+    while True:
+        get_pairings()
+        if cycle % 3 == 0:
+            get_premium()
+
+        time.sleep(random.randrange(15, 35))
+        cycle += 1
+
 #==== main ====
-VERSION = 'v1.1.3'
+VERSION = 'v2.0.0'
 update()
 
+
+if night_cont:
+    sbc.set_brightness(0)
 
 print('[System] Waiting 10 seconds')
 time.sleep(10)
 print('[System] Performing setup')
-
-#set up variables
-night_cont = False
-img_reload_initial = [None, None]
-prem_vertical_offset = 0
-rsa_vertical_offset = 0
-loc_mousehide = [[50, 295], [737, 300]]
-img_blank = None
-rsa_blank = None
-filter_prem = False
 
 #load config file
 with open('config.json', 'r') as file:
@@ -227,73 +213,8 @@ with open('config.json', 'r') as file:
 system_mode = 'dev' if pyautogui.size().width == 1920 else 'release'
 print('[System] Loading in {} mode'.format(system_mode))
 
-bb_reload = config[system_mode]['bb_reload']
-loc_reload = config[system_mode]['loc_reload']
-bb_prem_vertprobe = config[system_mode]['bb_prem_vertprobe']
-prem_vertprobe_goal = config[system_mode]['prem_vertprobe_goal']
-bb_prem_name = config[system_mode]['bb_prem_name']
-bb_prem_icon = config[system_mode]['bb_prem_icon']
-prem_row_height = config[system_mode]['prem_row_height']
-prem_num_rows = config[system_mode]['prem_num_rows']
-bb_rsa_vertprobe = config[system_mode]['bb_rsa_vertprobe']
-rsa_vertprobe_goal = config[system_mode]['rsa_vertprobe_goal']
-bb_rsa = config[system_mode]['bb_rsa']
-rsa_row_height = config[system_mode]['rsa_row_height']
-rsa_num_rows = config[system_mode]['rsa_num_rows']
-option_monitor_rsa = config[system_mode]['monitor_rsa']
-
-rsa_interval = config['rsa_interval']
-
-
-# find vertical offset
-pyautogui.click(loc_mousehide[0])
-img_vertprobe = ImageGrab.grab(bbox=bb_prem_vertprobe)
-for y in range(0, 100):
-    if img_vertprobe.getpixel((0, y)) == (128, 128, 128): # color of top of table
-        if not y == prem_vertprobe_goal:
-            bb_prem_name[1] += y - prem_vertprobe_goal
-            bb_prem_name[3] += y - prem_vertprobe_goal
-            bb_prem_icon[1] += y - prem_vertprobe_goal
-            bb_prem_icon[3] += y - prem_vertprobe_goal
-        break
-
-
-
-pyautogui.click(loc_mousehide[1])
-img_vertprobe = ImageGrab.grab(bbox=bb_rsa_vertprobe)
-for y in range(0, 100):
-    if img_vertprobe.getpixel((0, y)) == (128, 128, 128): # color of top of table
-        bb_rsa[1] = 350 + y + 62
-        bb_rsa[3] = 350 + y + 62 + 7
-        break
-
-
-realign()
-
-if night_cont:
-    sbc.set_brightness(0)
-
-for i in range(0, prem_num_rows):
-    ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i)).save('debug/name_{}.png'.format(i))
-    ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i)).save('debug/icon_{}.png'.format(i))
-for i in range(0, rsa_num_rows):
-    ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i)).save('debug/rsa_{}.png'.format(i))
-
-
 pyautogui.click(loc_mousehide[0])
 img_reload_initial[0] = ImageGrab.grab(bbox=bb_reload[0])
 time.sleep(0.2)
-pyautogui.click(loc_mousehide[1])
-img_reload_initial[1] = ImageGrab.grab(bbox=bb_reload[1])
-time.sleep(0.2)
 
-img_blank = ImageGrab.grab(bbox=(bb_prem_icon[0] + 50, bb_prem_icon[1], bb_prem_icon[2] + 50, bb_prem_icon[3]))
-img_blank.save('debug/blank.png')
-rsa_blank = ImageGrab.grab(bbox=(bb_rsa[0] + 400, bb_rsa[1], bb_rsa[2] + 400, bb_rsa[3]))
-rsa_blank.save('debug/rsa_blank.png')
-
-print('[System] Setup complete')
-MonitorWindow()
-
-
-#==== end of program ====
+main()
---
+++ main_old.py
@@ -0,0 +1,299 @@
+import json
+import time
+import random
+import winsound
+import shutil
+import os
+import sys
+from zipfile import ZipFile
+
+import requests
+from PIL import ImageGrab
+from PIL import ImageChops
+import pyautogui
+import screen_brightness_control as sbc
+import keyboard
+
+
+def IndexOfImage(imageArray, img):
+    for i in range(len(imageArray)):
+        if not ImageChops.difference(img, imageArray[i]).getbbox():
+            return i
+    return -1
+
+
+def Pause():
+    print('[System] Program paused')
+    response = pyautogui.confirm(text='Select option on how to continue', title='Program paused', buttons=['Continue', 'Exit'])
+    if response == 'Exit':
+        sys.exit()
+        os._exit()
+        quit()
+        exit()
+
+def Alert(msg, silent=False):
+    print('[Alert]', msg)
+    if night_cont:
+        sbc.set_brightness(30)
+    winsound.PlaySound('alert_sound.wav', winsound.SND_LOOP|winsound.SND_ASYNC)
+
+    response = pyautogui.confirm(text=msg, title='Select option on how to continue', buttons=['Continue', 'Pause', 'Exit'])
+
+    winsound.PlaySound(None, 0)
+
+    if response == 'Pause':
+        Pause()
+    elif response == 'Exit':
+        os._exit()
+    if night_cont:
+        sbc.set_brightness(0)
+
+
+#reload and wait page on left or right of screen (0, 1), return True or False if successful
+def ReloadAndWait(window):
+    print('[System] Reloading {} window'.format('Premium' if window == 0 else 'Rsa'))
+    pyautogui.click(loc_reload[window])
+    time.sleep(0.5)
+    pyautogui.moveTo(loc_mousehide[window])
+
+    i = 6000
+
+    time.sleep(0.5)
+    while ImageChops.difference(ImageGrab.grab(bbox=bb_reload[window]), img_reload_initial[window]).getbbox() and i > 0:
+        print('[System] {} window has not loaded yet, waiting'.format('Premium' if window == 0 else 'Rsa'), str(i), 'more seconds')
+        i -= 1
+        time.sleep(1)
+
+    return i > 10
+
+
+def Wait(seconds):
+    print('[System] Waiting', str(seconds), 'seconds')
+    target = time.time() + seconds
+    while time.time() < target:
+        if keyboard.is_pressed('p'):
+            Pause()
+        #time.sleep(0.5)
+
+#keep track of each rows id and premium status. each iteration retrieve new trip info then for each premium trip found check if it was present in last iteration
+#if not then alert user
+#
+def MonitorWindow():
+    img_names = [0] * prem_num_rows
+    img_icons = [0] * prem_num_rows
+    img_names_last = [0] * prem_num_rows
+    img_icons_last = [0] * prem_num_rows
+
+    img_rsa = [0] * rsa_num_rows
+    img_rsa_last = [0] * rsa_num_rows
+
+    num_refreshes = 0
+
+    pyautogui.click(loc_mousehide[0])
+    for i in range(0, prem_num_rows):
+        img_names[i] = ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i))
+        img_icons[i] = ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i))
+
+    pyautogui.click(loc_mousehide[1])
+    for i in range(0, rsa_num_rows):
+        img_rsa[i] = ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i))
+
+    while True:
+        realign()
+        play_alert = False
+        linesfound = []
+        img_names_last = img_names.copy()
+        img_icons_last = img_icons.copy()
+        pyautogui.click(loc_mousehide[0])
+        for i in range(0, prem_num_rows):
+            img_names[i] = ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i))
+            img_icons[i] = ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i))
+
+            #just look for empty image. ned to make custom index of function to check for image equality.
+            # row contains premium trip where it did not before
+            if ImageChops.difference(img_blank, img_icons[i]).getbbox() or not filter_prem:
+                if filter_prem:
+                    print('[Debug] Trip on row', str(i), 'is premium, previously found on row ', end='')
+                    img_old_index = IndexOfImage(img_names_last, img_names[i])
+                    print(str(img_old_index))
+                # trip existed before but was not premium
+                if img_old_index > -1:
+                    if not ImageChops.difference(img_icons_last[img_old_index], img_blank).getbbox():
+                        play_alert = True
+                        if filter_prem:
+                            linesfound.append(i + 1)
+                        #Alert("Trip went Premium")
+                    #else:
+                        #Alert('Trip stayed premium')
+                else:
+                    play_alert = True
+                    linesfound.append(i + 1)
+                    #Alert("New Premium trip added")
+
+        if play_alert:
+            Alert('Premium trip found on line(s) {}'.format(str(linesfound)))
+            play_alert = False
+
+        print(f'[System] Refresh count: {num_refreshes}')
+        Wait(random.randrange(2, 6))
+        if num_refreshes % 15 == 14:
+            ReloadAndWait(0)
+
+        num_refreshes = num_refreshes + 1
+
+        if option_monitor_rsa:
+            realign()
+            img_rsa_last = img_rsa.copy()
+            pyautogui.click(loc_mousehide[1])
+            for i in range(0, rsa_num_rows):
+                img_rsa[i] = ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i))
+                img_old_index = IndexOfImage(img_rsa_last, img_rsa[i])
+                if img_old_index == -1 and ImageChops.difference(rsa_blank, img_rsa[i]).getbbox():
+                    play_alert = True
+                    linesfound.append(i + 1)
+
+            if play_alert:
+                Alert('Rsa found on line(s) {}'.format(str(linesfound)))
+                play_alert = False
+
+            if num_refreshes % rsa_interval == 0 and option_monitor_rsa:
+                print(f'[System] Refresh count: {num_refreshes}')
+                Wait(random.randrange(10, 50))
+                ReloadAndWait(1)
+                num_refreshes = num_refreshes + 1
+
+def realign():
+    return
+    img_horizprobe = ImageGrab.grab(bbox=(0, bb_prem_icon[1], 600, bb_prem_icon[1] + 1))
+    for x in range(560, 450, -1):
+        if img_horizprobe.getpixel((x, 0)) == (128, 128, 128):
+            bb_prem_icon[0] = x - 20
+            bb_prem_icon[2] = x - 10
+            break
+
+def update():
+    print(f'[System] Checking for updates, current version {VERSION}')
+
+    dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+    if os.path.exists(f'{dir}\\temp'):
+        shutil.rmtree(f'{dir}\\temp')
+
+    url = "https://api.github.com/repos/pokeylink227/godzilla/releases/latest"
+    response = requests.get(url)
+
+    if response.json()['tag_name'] > VERSION:
+        print('[System] Update found')
+        r = requests.get(response.json()['assets'][0]['browser_download_url'])
+        f = open('update.zip','wb')
+        f.write(r.content)
+        f.close()
+
+        with ZipFile('update.zip') as zpf:
+            zpf.extractall()
+
+        shutil.move(f'{dir}\\main\\main', f'{dir}\\temp')
+
+        f = open(f'{dir}/upd.bat', 'w')
+        f.write(f'@echo off  \ncd {dir}\nrmdir /s /q {dir}\\main  \ntimeout 2 >nul\nrename {dir}\\temp main  \ntimeout 2 >nul\ndel {dir}\\upd.bat') #add absolute paths
+        f.close()
+        os.startfile(f'{dir}/upd.bat')
+
+        sys.exit()
+    print('[System] Program up to date')
+
+#==== main ====
+VERSION = 'v1.1.3'
+update()
+
+
+print('[System] Waiting 10 seconds')
+time.sleep(10)
+print('[System] Performing setup')
+
+#set up variables
+night_cont = False
+img_reload_initial = [None, None]
+prem_vertical_offset = 0
+rsa_vertical_offset = 0
+loc_mousehide = [[50, 295], [737, 300]]
+img_blank = None
+rsa_blank = None
+filter_prem = False
+
+#load config file
+with open('config.json', 'r') as file:
+    config = json.load(file)
+system_mode = 'dev' if pyautogui.size().width == 1920 else 'release'
+print('[System] Loading in {} mode'.format(system_mode))
+
+bb_reload = config[system_mode]['bb_reload']
+loc_reload = config[system_mode]['loc_reload']
+bb_prem_vertprobe = config[system_mode]['bb_prem_vertprobe']
+prem_vertprobe_goal = config[system_mode]['prem_vertprobe_goal']
+bb_prem_name = config[system_mode]['bb_prem_name']
+bb_prem_icon = config[system_mode]['bb_prem_icon']
+prem_row_height = config[system_mode]['prem_row_height']
+prem_num_rows = config[system_mode]['prem_num_rows']
+bb_rsa_vertprobe = config[system_mode]['bb_rsa_vertprobe']
+rsa_vertprobe_goal = config[system_mode]['rsa_vertprobe_goal']
+bb_rsa = config[system_mode]['bb_rsa']
+rsa_row_height = config[system_mode]['rsa_row_height']
+rsa_num_rows = config[system_mode]['rsa_num_rows']
+option_monitor_rsa = config[system_mode]['monitor_rsa']
+
+rsa_interval = config['rsa_interval']
+
+
+# find vertical offset
+pyautogui.click(loc_mousehide[0])
+img_vertprobe = ImageGrab.grab(bbox=bb_prem_vertprobe)
+for y in range(0, 100):
+    if img_vertprobe.getpixel((0, y)) == (128, 128, 128): # color of top of table
+        if not y == prem_vertprobe_goal:
+            bb_prem_name[1] += y - prem_vertprobe_goal
+            bb_prem_name[3] += y - prem_vertprobe_goal
+            bb_prem_icon[1] += y - prem_vertprobe_goal
+            bb_prem_icon[3] += y - prem_vertprobe_goal
+        break
+
+
+
+pyautogui.click(loc_mousehide[1])
+img_vertprobe = ImageGrab.grab(bbox=bb_rsa_vertprobe)
+for y in range(0, 100):
+    if img_vertprobe.getpixel((0, y)) == (128, 128, 128): # color of top of table
+        bb_rsa[1] = 350 + y + 62
+        bb_rsa[3] = 350 + y + 62 + 7
+        break
+
+
+realign()
+
+if night_cont:
+    sbc.set_brightness(0)
+
+for i in range(0, prem_num_rows):
+    ImageGrab.grab(bbox=(bb_prem_name[0], bb_prem_name[1] + prem_row_height * i, bb_prem_name[2], bb_prem_name[3] + prem_row_height * i)).save('debug/name_{}.png'.format(i))
+    ImageGrab.grab(bbox=(bb_prem_icon[0], bb_prem_icon[1] + prem_row_height * i, bb_prem_icon[2], bb_prem_icon[3] + prem_row_height * i)).save('debug/icon_{}.png'.format(i))
+for i in range(0, rsa_num_rows):
+    ImageGrab.grab(bbox=(bb_rsa[0], bb_rsa[1] + rsa_row_height * i, bb_rsa[2], bb_rsa[3] + rsa_row_height * i)).save('debug/rsa_{}.png'.format(i))
+
+
+pyautogui.click(loc_mousehide[0])
+img_reload_initial[0] = ImageGrab.grab(bbox=bb_reload[0])
+time.sleep(0.2)
+pyautogui.click(loc_mousehide[1])
+img_reload_initial[1] = ImageGrab.grab(bbox=bb_reload[1])
+time.sleep(0.2)
+
+img_blank = ImageGrab.grab(bbox=(bb_prem_icon[0] + 50, bb_prem_icon[1], bb_prem_icon[2] + 50, bb_prem_icon[3]))
+img_blank.save('debug/blank.png')
+rsa_blank = ImageGrab.grab(bbox=(bb_rsa[0] + 400, bb_rsa[1], bb_rsa[2] + 400, bb_rsa[3]))
+rsa_blank.save('debug/rsa_blank.png')
+
+print('[System] Setup complete')
+MonitorWindow()
+
+
+#==== end of program ====
