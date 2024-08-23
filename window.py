4cbee0ea10e5a933dac9e8137a6fac5de1a6d806
PokeyLink227 <thomasrice7@yahoo.com>
2024-08-23 17:51:59 -0400

    added started input field

--- window.py
+++ window.py
@@ -1,8 +1,7 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Header, Footer, Static, Button, Log, Placeholder
+from textual.widgets import Header, Footer, Static, Button, Log, Input
 from textual.containers import Container
 import time
-
 
 class GodzillaApp(App):
     """A Textual app to manage stopwatches."""
@@ -18,20 +17,38 @@ class GodzillaApp(App):
             self.num_refreshes += 1
             self.query_one("#stats").update(f"{self.num_refreshes}")
 
+
+    def on_input_submitted(self, event):
+        text = self.query_one(Input)
+        self.query_one("#log").write_line(text.value)
+        text.value = ""
+
+
     def compose(self) -> ComposeResult:
         """Create child widgets for the app."""
         yield Header()
         yield Footer()
+        log = Log("LOG", id="log")
+        log.border_title = "Program Log"
+        ignore = Static("IGNORE", id="ignored-list")
+        ignore.border_title = "Ignored"
+        watch = Static("WATCH", id="watch-list")
+        watch.border_title = "Watch List"
+        stats = Static(f"Total refreshes: {self.num_refreshes}\n\n", id="stats")
+        stats.border_title = "Stats"
+        input_area = Input(placeholder="Enter Command here", id="input-area")
+        input_area.border_title = "Input"
+
         yield Container(
             Container(
-                Log("LOG", id="log"),
-                Static("IGNORE", id="ignored-list"),
-                Static("WATCH", id="watch-list"),
+                log,
+                ignore,
+                watch,
                 id="top-row"
             ),
             Container(
-                Static(f"{self.num_refreshes}", id="stats"),
-                Static("INPUT", id="input-area"),
+                stats,
+                input_area,
                 Container(
                     Button("meow", id="start"),
                     id="buttons"
